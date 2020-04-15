46406c8967cf1d9be43cd76171d80a0e2b1a6bb0
Grigorii Balabanov <810411@mail.ru>
2020-04-15 12:07:08 +0500

    store component

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -3,10 +3,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {ModelModule} from '../model/model.module';
 import {StoreComponent} from './store.component';
+import {CounterDirective} from './counter.directive';
 
 @NgModule({
   declarations: [
-    StoreComponent
+    StoreComponent,
+    CounterDirective
   ],
   imports: [
     BrowserModule,
